cf5326d1ba1483153cb96be1aca0499a6a164b8b
gilles zwijsen <gilles.zwijsen@student.kdg.be>
2023-04-30 01:15:09 +0200

    less gass in character.sol by mapping instead of array

--- web3/contracts/Characters.sol
+++ web3/contracts/Characters.sol
@@ -23,50 +23,28 @@ contract Characters {
     Champs[] camps;
 
 
+    mapping(uint => Champs) public champsMap;
 
-
-
-
-    Champs private Defender = Champs(1,2,1,1,"defender","Give adjusted minions +1/+1 and taunt",false,false,false,false,false,true);
-    Champs private Fletchling = Champs(3,3,3,2,"fletchling","After this minon attacks a hero, Adapt",false,false,false,false,false,true);
-    Champs private Yeti = Champs(5,3,4,3,"Yeti","",false,false,false,false,false,true);
-    Champs private Scrab = Champs(3,2,2,4,"Scrab","Deathrattle: gain 2 armor",false,false,false,false,false,true);
-    Champs private Deathwing = Champs(12,12,5,5,"Deathwing","Battlecry: Destroy all other minions and discard your hand",false,false,false,false,false,true);
-    Champs public Invincible = Champs(4,5,7,6,"invincible","Reborn,Battlecry and Deathrattle: Give a random friendly minion +5/+5 and Taunt",false,false,false,false,false,true);
-    Champs public Brian = Champs(4,2,3,7,"Brian BronzeBeard","Your battlecry trigger twice",false,false,false,false,false,true);
-    Champs public Shrinkmeister = Champs(2,3,2,8,"Shrinkmeister","Give a minion -2 Attack",false,false,false,false,false,true);
-    Champs public CrazedWretch = Champs(4,1,2,9,"Crazed Wretch","Has +2 Attack and Charge while damaged",false,false,false,false,false,true);
-    Champs public CrazedAlchemist = Champs(2,2,2,10,"Crazed Alchemist","Battlecry: Swap the Attack and Health of a minion",false,false,false,false,false,true);
-    Champs public ZombieChow = Champs(3,2,1,11,"Zombie Chow","Deathrattle: Restore 5 Health to the enemy Hero",false,false,false,false,false,true);
-    Champs public Gruul = Champs(7,7,8,12,"Gruul","at the end of each turn gain +1/+1",false,false,false,false,false,true);
-    Champs public SpawnOfNzoth = Champs(2,2,3,13,"Spawn of N'Zoth","Deathrattle: give your minions +1/+1",false,false,false,false,false,true);
-    Champs public LeperGnome = Champs(1,2,1,14,"Leper Gnome","Deathrattle: Deal 2 damage to the enemy hero",false,false,false,false,false,true);
-    Champs public DerangedDocter = Champs(8,8,8,15,"Deranged Doctor","Deathrattle: Restore 8 health to your hero",false,false,false,false,false,true);
-    Champs public Mistress = Champs(2,2,1,16,"Mistress of Mixtures","Deathrattle: Restore 4 healt to each hero",false,false,false,false,false,true);
-    Champs public Grizzly = Champs(3,3,3,17,"Ironfur Grizzly","taunt",true,false,false,false,false,true);
 
 
 
   
-    constructor(){
-          camps.push(Defender);
-             camps.push(Fletchling);
-                camps.push(Yeti);
-                   camps.push(Scrab);
-                      camps.push(Deathwing);
-                         camps.push(Invincible);
-                            camps.push(Brian);
-                                 camps.push(Shrinkmeister);
-                                      camps.push(CrazedWretch);
-                                           camps.push(CrazedAlchemist);
-                                             camps.push(ZombieChow);
-                                                  camps.push(Gruul);
-                                                       camps.push(SpawnOfNzoth);
-                                                        //   camps.push(LeperGnome);
-                                                        //      camps.push(DerangedDocter);
-                                                        //        camps.push(Mistress);
-                                                        //             camps.push(Grizzly);
-    }
+   constructor() public {
+    champsMap[0] = Champs(0, 0, 0, 0, "", "", false, false, false, false, false, false);
+    champsMap[1] = Champs(1, 2, 1, 1, "Defender", "Give adjusted minions +1/+1 and taunt", false, false, false, false, false, true);
+    champsMap[2] = Champs(3, 3, 3, 2, "Fletchling", "After this minion attacks a hero, Adapt", false, false, false, false, false, true);
+    champsMap[3] = Champs(5, 3, 4, 3, "Yeti", "", false, false, false, false, false, true);
+    champsMap[4] = Champs(3, 2, 2, 4, "Scrab", "Deathrattle: gain 2 armor", false, false, false, false, false, true);
+    champsMap[5] = Champs(12, 12, 5, 5, "Deathwing", "Battlecry: Destroy all other minions and discard your hand", false, false, false, false, false, true);
+    champsMap[6] = Champs(4, 5, 7, 6, "Invincible", "Reborn, Battlecry and Deathrattle: Give a random friendly minion +5/+5 and Taunt", false, false, false, false, false, true);
+    champsMap[7] = Champs(4, 2, 3, 7, "Brian BronzeBeard", "Your battlecry trigger twice", false, false, false, false, false, true);
+    champsMap[8] = Champs(2, 3, 2, 8, "Shrinkmeister", "Give a minion -2 Attack", false, false, false, false, false, true);
+    champsMap[9] = Champs(4, 1, 2, 9, "Crazed Wretch", "Has +2 Attack and Charge while damaged", false, false, false, false, false, true);
+    champsMap[10] = Champs(2, 2, 2, 10, "Crazed Alchemist", "Battlecry: Swap the Attack and Health of a minion", false, false, false, false, false, true);
+    champsMap[11] = Champs(3, 2, 1, 11, "Zombie Chow", "Deathrattle: Restore 5 Health to the enemy Hero", false, false, false, false, false, true);
+    champsMap[12] = Champs(7, 7, 8, 12, "Gruul", "at the end of each turn gain +1/+1", false, false, false, false, false, true);
+    champsMap[13] = Champs(2, 2, 3, 13, "Spawn of N'Zoth", "Deathrattle: give your minions +1/+1", false, false, false, false, false, true);
+   }
 
  
 
@@ -77,7 +55,7 @@ contract Characters {
 
 
     function seperateValues(uint _x) internal view  returns (uint8,uint8,uint8,uint8,string memory,string memory,bool,bool,bool,bool,bool,bool) {
-        Champs memory x = camps[_x];
+        Champs memory x = champsMap[_x];
         return (x.health,x.attack,x.manaCost,x.id,x.name,x.description,x.taunt,x.windfury,x.divineshield,x.stealth,x.poisonious,x.asleep);
     }
 
